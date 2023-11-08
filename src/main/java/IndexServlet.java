bab716d07d05c61889438a476a39b3b427a644e1
Natalia <PalejNatalia@gmail.com>
2023-11-08 21:54:49 +0000

    Working on displaying details from database in the jsp

--- src/main/java/IndexServlet.java
+++ src/main/java/IndexServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class DogsController
@@ -18,23 +19,26 @@ public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// Create dogsList which uses instance of DogsDAO and returns with list()
-		List<Dogs> dogsList = null;
-		
-		try {
-			dogsList = DogsDAO.instance.dogsList();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		// Add list of dogs attributes to the request
-		request.setAttribute("listOfDogs", dogsList);
-		
-		// Forward updated request and response to display.jsp view
-		request.getRequestDispatcher("index.jsp").forward(request, response);
+		HttpSession session = request.getSession();
+	    String userEmail = (String) session.getAttribute("email");
+	    
+	    if (userEmail != null) {
+	        // Get dogs associated with the logged-in user's email
+	        List<Dogs> usersDog;
+			try {
+				usersDog = DogsDAO.instance.getDogByUserEmail(userEmail);
+				
+				// Add the list of dogs to the request
+		        request.setAttribute("dogsList", usersDog);
+
+		        // Forward the request to index.jsp
+		        request.getRequestDispatcher("index.jsp").forward(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}       
+	    } else {
+	        response.sendRedirect("login.jsp");
+	    }
 	}
 
 }
